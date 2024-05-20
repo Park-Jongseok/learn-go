d99385cd8d49c832b116f86c0f06ba109c5f2053
Park-Jongseok <pjongsuk1@gmail.com>
2024-05-20 20:30:09 +0900

    다수의 반환값 받기

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func multiply(a, b int) int {
-	return a * b
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
 func main() {
-	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("mike")
+	fmt.Println(totalLength, upperName)
 }
