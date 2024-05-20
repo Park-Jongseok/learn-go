7c45315016e1543200e140089b1134dfdb649371
Park-Jongseok <pjongsuk1@gmail.com>
2024-05-20 20:17:11 +0900

    변수 선언 및 타입 지정

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"hello"
+)
+
 func main() {
 	// Error
-	const constant string = "Hello, World!"
-	constant = "World! Hello"
+	// const constant string = "Hello, World!"
+	// constant = "World! Hello"
+
+	var message string = hello.Hello()
+	message = "World! Hello"
+	fmt.Println(message)
 }
