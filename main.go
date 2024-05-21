37f2f4336b913647dc8c273a71be183d43ba4027
Park-Jongseok <pjongsuk1@gmail.com>
2024-05-21 18:46:07 +0900

    불필요한 부분 삭제

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case koreanAge:
-		return false
-	default:
-		return true
-	}
-}
 func main() {
-	fmt.Println(canIDrink(16))
 }
